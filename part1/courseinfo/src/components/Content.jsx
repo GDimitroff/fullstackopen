fe8fe3b4a46c9f86ddae1419bda6ca7d08c4f4ba
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-08 09:21:28 +0300

    Refactor App component to use objects for parts and exercises

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,26 +1,19 @@
 import Part from './Part';
 
-const Content = ({
-  part1,
-  exercises1,
-  part2,
-  exercises2,
-  part3,
-  exercises3
-}) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <div>
       <Part
-        part={part1}
-        exercise={exercises1}
+        part={part1.name}
+        exercise={part1.exercises}
       />
       <Part
-        part={part2}
-        exercise={exercises2}
+        part={part2.name}
+        exercise={part2.exercises}
       />
       <Part
-        part={part3}
-        exercise={exercises3}
+        part={part3.name}
+        exercise={part3.exercises}
       />
     </div>
   );
