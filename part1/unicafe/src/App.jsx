51ca7b4794f30e07bfdf3f12f6229337f643e6fd
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-09 16:30:49 +0300

    Add more statistics - all, average, percentage of positive feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,6 +7,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+  const average = (good - bad) / all || 0;
+  const positive = (good / all) * 100 || 0;
+
   return (
     <section>
       <article>
@@ -30,6 +34,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
       </article>
     </section>
   );
