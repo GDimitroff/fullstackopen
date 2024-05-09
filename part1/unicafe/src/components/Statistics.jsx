1a797993a5e349eadc4e4436fd04ae01a2d728e7
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-09 16:41:25 +0300

    Add fallback to display statistics only if feedback has been gathered

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,4 +1,5 @@
 const Statistics = ({ good, neutral, bad }) => {
+  const hasFeedback = good || neutral || bad;
   const all = good + neutral + bad;
   const average = (good - bad) / all || 0;
   const positive = (good / all) * 100 || 0;
@@ -6,12 +7,18 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <article>
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      {hasFeedback ? (
+        <>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive} %</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </article>
   );
 };
