75bec12e4b6196c48ec744513599e83366c31d4a
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-10 17:11:37 +0300

    Implement search field that can be used to filter the list of people

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const personsToShow = persons.filter((person) => {
+    return person.name.toLowerCase().includes(filter.toLowerCase());
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -25,6 +33,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{' '}
+        <input
+          type='text'
+          name='filter'
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+
+      <h2>Add new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:{' '}
@@ -48,9 +67,10 @@ const App = () => {
           <button type='submit'>add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
+      {personsToShow.map((person) => (
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
