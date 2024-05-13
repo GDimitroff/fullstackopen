95c64b470eb7bf15d4bfcd5517971ae0790735e0
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-13 20:17:17 +0300

    Fully connect database with the backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,19 +7,14 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-
-morgan.token('post-body', (req, res) => JSON.stringify(req.body));
-
+app.use(express.static('dist'));
 app.use(express.json());
+morgan.token('post-body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(
     ':method :url :status :res[content-length] - :response-time ms :post-body'
   )
 );
-app.use(express.static('dist'));
 
 app.get('/info', (request, response) => {
   const date = new Date();
@@ -44,39 +39,71 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  // TODO: fix this
-  // if (persons.find((person) => person.name === name)) {
-  //   return response.status(400).json({
-  //     error: 'name must be unique'
-  //   });
-  // }
-
   const person = new Person({
     name,
     number
   });
 
-  // TODO: error handling
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  // TODO: handle 404
-  // response.status(404).end();
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-// TODO: implement delete route
-// response.status(204).end();
-app.delete('/api/persons/:id', (request, response) => {
-  console.log('delete', request.params.id);
+app.put('/api/persons/:id', (request, response) => {
+  const { name, number } = request.body;
+
+  const person = {
+    name,
+    number
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
