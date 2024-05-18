c25c1f6d6705a8d0af37de239d430aa955a07120
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-18 19:21:17 +0300

    Install and configure morgan, chalk

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,12 +1,17 @@
+const morgan = require('morgan');
+const chalk = require('chalk');
 const logger = require('./logger');
 
-const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method);
-  logger.info('Path:  ', request.path);
-  logger.info('Body:  ', request.body);
-  logger.info('---');
-  next();
-};
+morgan.token('post-body', (req, res) => JSON.stringify(req.body));
+const requestLogger = morgan((tokens, req, res) => {
+  return [
+    chalk.cyan(tokens.method(req, res)),
+    chalk.greenBright(tokens.url(req, res)),
+    chalk.magentaBright(tokens.status(req, res)),
+    chalk.grey(tokens['response-time'](req, res) + ' ms'),
+    chalk.whiteBright(tokens['post-body'](req, res))
+  ].join(' ');
+});
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
