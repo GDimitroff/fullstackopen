9a99c82ef3c8724e7292b7c3deaca81d8463c06b
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-19 15:47:11 +0300

    Refactor tests

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -24,7 +24,10 @@ mongoose
 app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
-app.use(middleware.requestLogger);
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(middleware.requestLogger);
+}
 
 app.use('/api/blogs', blogsRouter);
 
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,111 +1,111 @@
-const { test, after, beforeEach } = require('node:test');
+const { test, after, beforeEach, describe } = require('node:test');
 const assert = require('node:assert');
-const Blog = require('../models/blog');
 const mongoose = require('mongoose');
-const helper = require('./test_helper');
 const supertest = require('supertest');
 const app = require('../app');
-
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
+const helper = require('./test_helper');
 
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
+const Blog = require('../models/blog');
 
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
-});
+describe.only('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+  });
 
-test.only('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+  test.only('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test.only('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
+  test.only('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length);
-});
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  });
 
-test.only('the first blog is about React patterns', async () => {
-  const response = await api.get('/api/blogs');
+  test.only('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs');
 
-  const titles = response.body.map((e) => e.title);
-  assert.strictEqual(titles.includes('React patterns'), true);
-});
+    const titles = response.body.map((e) => e.title);
+    assert.strictEqual(titles.includes('React patterns'), true);
+  });
 
-test.only('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'Testing patterns',
-    author: 'Tests are funny',
-    url: 'https://testingtpatterns.com/',
-    likes: 7
-  };
+  describe.only('addition of a new blog', () => {
+    test.only('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'Testing patterns',
+        author: 'Tests are funny',
+        url: 'https://testingtpatterns.com/',
+        likes: 7
+      };
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
 
-  const titles = blogsAtEnd.map((b) => b.title);
-  assert(titles.includes('Testing patterns'));
-});
+      const titles = blogsAtEnd.map((b) => b.title);
+      assert(titles.includes('Testing patterns'));
+    });
 
-test.only('verify that the unique identifier property of the blog posts is named id and not _id', async () => {
-  const response = await api.get('/api/blogs');
+    test.only('verify that the unique identifier property is named id', async () => {
+      const response = await api.get('/api/blogs');
 
-  assert(response.body[0].id);
-  assert.strictEqual(response.body[0]._id, undefined);
-});
+      assert(response.body[0].id);
+      assert.strictEqual(response.body[0]._id, undefined);
+    });
 
-test.only('if the likes property is missing from the request, it will default to the value 0', async () => {
-  const newBlog = {
-    title: 'Likes missing',
-    author: 'no likes :(',
-    url: 'https://testingtpatterns.com/'
-  };
+    test.only('verify that if missing the default value of likes is 0', async () => {
+      const newBlog = {
+        title: 'Likes missing',
+        author: 'no likes :(',
+        url: 'https://testingtpatterns.com/'
+      };
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-  assert.strictEqual(response.body.likes, 0);
-});
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+      assert.strictEqual(response.body.likes, 0);
+    });
 
-test.only('if the title property is missing from the request', async () => {
-  const newBlog = {
-    author: 'no likes :(',
-    url: 'https://testingtpatterns.com/'
-  };
+    test.only('fails with status code 400 if title property is missing', async () => {
+      const newBlog = {
+        author: 'no likes :(',
+        url: 'https://testingtpatterns.com/'
+      };
 
-  await api.post('/api/blogs').send(newBlog).expect(400);
+      await api.post('/api/blogs').send(newBlog).expect(400);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
-});
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+    });
 
-test.only('if the url property is missing from the request', async () => {
-  const newBlog = {
-    title: 'Likes missing',
-    author: 'no likes :('
-  };
+    test.only('fails with status code 400 if url property is missing', async () => {
+      const newBlog = {
+        title: 'Likes missing',
+        author: 'no likes :('
+      };
 
-  await api.post('/api/blogs').send(newBlog).expect(400);
+      await api.post('/api/blogs').send(newBlog).expect(400);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+    });
+  });
 });
 
 after(async () => {
