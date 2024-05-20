9b68745b90525d4c7babe4124adfcf85256d2a24
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-20 08:42:43 +0300

    Refactor tests

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -9,34 +9,34 @@ const helper = require('./helper');
 
 const Blog = require('../models/blog');
 
-describe.only('when there is initially some blogs saved', () => {
+describe('when there is initially some blogs saved', () => {
   beforeEach(async () => {
     await Blog.deleteMany({});
     await Blog.insertMany(helper.initialBlogs);
   });
 
-  test.only('blogs are returned as json', async () => {
+  test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/);
   });
 
-  test.only('all blogs are returned', async () => {
+  test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs');
 
     assert.strictEqual(response.body.length, helper.initialBlogs.length);
   });
 
-  test.only('a specific blog is within the returned blogs', async () => {
+  test('a specific blog is within the returned blogs', async () => {
     const response = await api.get('/api/blogs');
 
     const titles = response.body.map((e) => e.title);
     assert.strictEqual(titles.includes('React patterns'), true);
   });
 
-  describe.only('viewing a specific blog', () => {
-    test.only('succeeds with a valid id', async () => {
+  describe('viewing a specific blog', () => {
+    test('succeeds with a valid id', async () => {
       const blogsAtStart = await helper.blogsInDb();
       const blogToView = blogsAtStart[0];
 
@@ -48,21 +48,21 @@ describe.only('when there is initially some blogs saved', () => {
       assert.deepStrictEqual(response.body, blogToView);
     });
 
-    test.only('fails with statuscode 404 if blog does not exist', async () => {
+    test('fails with statuscode 404 if blog does not exist', async () => {
       const validNonexistingId = await helper.nonExistingId();
 
       await api.get(`/api/blogs/${validNonexistingId}`).expect(404);
     });
 
-    test.only('fails with statuscode 400 if id is invalid', async () => {
+    test('fails with statuscode 400 if id is invalid', async () => {
       const invalidId = '5a3d5da59070081a82a3445';
 
       await api.get(`/api/blogs/${invalidId}`).expect(400);
     });
   });
 
-  describe.only('addition of a new blog', () => {
-    test.only('succeeds with valid data', async () => {
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
       const newBlog = {
         title: 'Testing patterns',
         author: 'Tests are funny',
@@ -83,14 +83,14 @@ describe.only('when there is initially some blogs saved', () => {
       assert(titles.includes('Testing patterns'));
     });
 
-    test.only('verify that the unique identifier property is named id', async () => {
+    test('verify that the unique identifier property is named id', async () => {
       const response = await api.get('/api/blogs');
 
       assert(response.body[0].id);
       assert.strictEqual(response.body[0]._id, undefined);
     });
 
-    test.only('verify that if missing the default value of likes is 0', async () => {
+    test('verify that if missing the default value of likes is 0', async () => {
       const newBlog = {
         title: 'Likes missing',
         author: 'no likes :(',
@@ -108,7 +108,7 @@ describe.only('when there is initially some blogs saved', () => {
       assert.strictEqual(response.body.likes, 0);
     });
 
-    test.only('fails with status code 400 if title property is missing', async () => {
+    test('fails with status code 400 if title property is missing', async () => {
       const newBlog = {
         author: 'no likes :(',
         url: 'https://testingtpatterns.com/'
@@ -120,7 +120,7 @@ describe.only('when there is initially some blogs saved', () => {
       assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
     });
 
-    test.only('fails with status code 400 if url property is missing', async () => {
+    test('fails with status code 400 if url property is missing', async () => {
       const newBlog = {
         title: 'Likes missing',
         author: 'no likes :('
@@ -133,8 +133,8 @@ describe.only('when there is initially some blogs saved', () => {
     });
   });
 
-  describe.only('updating a blog', () => {
-    test.only('succeeds with a valid id', async () => {
+  describe('updating a blog', () => {
+    test('succeeds with a valid id', async () => {
       const blogsAtStart = await helper.blogsInDb();
       const blogToUpdate = blogsAtStart[0];
 
@@ -148,20 +148,20 @@ describe.only('when there is initially some blogs saved', () => {
       assert.strictEqual(response.body.likes, 99);
     });
 
-    test.only('fails with statuscode 404 if blog does not exist', async () => {
+    test('fails with statuscode 404 if blog does not exist', async () => {
       const validNonexistingId = await helper.nonExistingId();
 
       await api.put(`/api/blogs/${validNonexistingId}`).expect(404);
     });
 
-    test.only('fails with status code 400 if id is invalid', async () => {
+    test('fails with status code 400 if id is invalid', async () => {
       const invalidId = '5a3d5da59070081a82a3445';
       await api.put(`/api/blogs/${invalidId}`).expect(400);
     });
   });
 
-  describe.only('deletion of a blog', () => {
-    test.only('succeeds with status code 204 if id is valid', async () => {
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
       const blogsAtStart = await helper.blogsInDb();
       const blogToDelete = blogsAtStart[0];
 
@@ -175,7 +175,7 @@ describe.only('when there is initially some blogs saved', () => {
       assert(!titles.includes(blogToDelete.title));
     });
 
-    test.only('fails with status code 400 if id is invalid', async () => {
+    test('fails with status code 400 if id is invalid', async () => {
       const invalidId = '5a3d5da59070081a82a3445';
       await api.delete(`/api/blogs/${invalidId}`).expect(400);
     });
