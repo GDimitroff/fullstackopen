e8c04ef98e55e2f83fc63157a3e6f72303042574
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-21 14:07:13 +0300

    add validation for username and password in user creation

--- part4/bloglist/tests/users_api.test.js
+++ part4/bloglist/tests/users_api.test.js
@@ -29,13 +29,13 @@ describe('when there is initially one user in db', () => {
     await user.save();
   });
 
-  test('creation succeeds with a fresh username', async () => {
+  test('creation succeeds with a valid user data', async () => {
     const usersAtStart = await helper.usersInDb();
 
     const newUser = {
-      username: 'mluukkai',
-      name: 'Matti Luukkainen',
-      password: 'salainen'
+      username: 'GDimitroff',
+      name: 'Gospodin Dimitrov',
+      password: 'bookworm'
     };
 
     await api
@@ -49,6 +49,119 @@ describe('when there is initially one user in db', () => {
 
     const usernames = usersAtEnd.map((u) => u.username);
     assert(usernames.includes(newUser.username));
+  });
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'admin'
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert(result.body.error.includes('expected `username` to be unique'));
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+
+  test('creation fails with proper statuscode and message if username is missing', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      name: 'Gospodin Dimitrov',
+      password: 'bookworm'
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert(result.body.error.includes('`username` is required'));
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+
+  test('creation fails with proper statuscode and message if username is too short', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'GD',
+      name: 'Gospodin Dimitrov',
+      password: 'bookworm'
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert(
+      result.body.error.includes(
+        'is shorter than the minimum allowed length (3)'
+      )
+    );
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+
+  test('creation fails with proper statuscode and message if password is missing', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'GDimitroff',
+      name: 'Gospodin Dimitrov'
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    assert(
+      result.body.error.includes('User validation failed: password is required')
+    );
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+
+  test('creation fails with proper statuscode and message if password too short', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'GDimitroff',
+      name: 'Gospodin Dimitrov',
+      password: 'bo'
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    assert(
+      result.body.error.includes(
+        'User validation failed: password is shorter than the minimum allowed length (3)'
+      )
+    );
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
   });
 });
 
