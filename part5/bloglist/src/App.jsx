33ee95c427a544a7ffc0fba512a3d1f4e30ca198
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-23 22:15:53 +0300

    add togglable component, refactor

--- part5/bloglist/src/App.jsx
+++ part5/bloglist/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 
 import blogService from './services/blogs';
 import Authentication from './components/Authentication';
 import Notification from './components/Notification';
 import Blogs from './components/Blogs';
-import CreateNewBlogForm from './components/CreateNewBlogForm';
+import BlogForm from './components/BlogForm';
+import Togglable from './components/Togglable';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
   const [blogs, setBlogs] = useState(null);
   const [notification, setNotification] = useState(null);
+  const blogFormRef = useRef();
 
   const setNotificationMessage = (type, message) => {
     setNotification({ type, message });
@@ -35,6 +37,20 @@ const App = () => {
     }
   }, []);
 
+  const handleCreateBlog = async (blogData) => {
+    try {
+      const newBlog = await blogService.create(blogData);
+      setBlogs((prevBlogs) => [...prevBlogs, newBlog]);
+      setNotificationMessage(
+        'success',
+        `a new blog ${newBlog.title} by ${newBlog.author} added`
+      );
+      blogFormRef.current.toggleVisibility();
+    } catch (error) {
+      setNotificationMessage('error', error.response.data.error);
+    }
+  };
+
   useEffect(() => {
     fetchBlogs();
     checkUser();
@@ -54,10 +70,9 @@ const App = () => {
       />
       {user && (
         <>
-          <CreateNewBlogForm
-            setBlogs={setBlogs}
-            setNotificationMessage={setNotificationMessage}
-          />
+          <Togglable buttonLabel="new blog" ref={blogFormRef}>
+            <BlogForm createBlog={handleCreateBlog} />
+          </Togglable>
           <hr />
           <Blogs blogs={blogs} />
         </>
---
+++ part5/bloglist/src/components/BlogForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+const BlogForm = ({ createBlog }) => {
+  const [blog, setBlog] = useState({ title: '', author: '', url: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBlog({ ...blog, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createBlog({
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+    });
+
+    setBlog({ title: '', author: '', url: '' });
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          title:
+          <input
+            type="text"
+            value={blog.title}
+            name="title"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={blog.author}
+            name="author"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="url"
+            value={blog.url}
+            name="url"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
