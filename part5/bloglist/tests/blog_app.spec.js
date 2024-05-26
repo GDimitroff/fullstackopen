5d3f30f732460e15d5d41ae4eeb52cd7d3987f4f
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-26 12:11:58 +0300

    tests: add authentication e2e tests

--- part5/bloglist/tests/blog_app.spec.js
+++ part5/bloglist/tests/blog_app.spec.js
@@ -1,19 +1,49 @@
 // @ts-check
 import { test, expect } from '@playwright/test'
+import { loginWith } from './helper'
 
 test.describe('blogs app', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'King Tester',
+        username: 'King',
+        password: 'king',
+      },
+    })
+
     await page.goto('http://localhost:5173')
   })
 
   test('login form is shown', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-
     const locator = page.getByRole('heading', {
       name: 'log in to application',
     })
     await expect(locator).toBeVisible()
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
+  })
+
+  test.describe('login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'King', 'king')
+      const locator = page.getByRole('heading', {
+        name: 'blogs',
+      })
+      await expect(locator).toBeVisible()
+      expect(page.getByText('King Tester logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'Dummy', 'dummy')
+      expect(
+        page.getByRole('heading', {
+          name: 'log in to application',
+        })
+      ).toBeVisible()
+      await expect(page.getByText('username')).toBeVisible()
+      await expect(page.getByText('password')).toBeVisible()
+    })
   })
 })
