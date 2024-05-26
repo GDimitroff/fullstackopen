58e793fd0627987abbf0922884298b2ea41c34fb
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-26 13:36:11 +0300

    chore: fix formatting

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,18 +1,18 @@
-const { Router } = require('express');
-const Blog = require('../models/blog');
-const { userExtractor } = require('../utils/middleware');
+const { Router } = require('express')
+const Blog = require('../models/blog')
+const { userExtractor } = require('../utils/middleware')
 
-const blogsRouter = Router();
+const blogsRouter = Router()
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 
-  response.json(blogs);
-});
+  response.json(blogs)
+})
 
 blogsRouter.post('/', userExtractor, async (request, response) => {
-  const { title, author, url, likes = 0 } = request.body;
-  const user = request.user;
+  const { title, author, url, likes = 0 } = request.body
+  const user = request.user
 
   const blog = new Blog({
     title,
@@ -20,40 +20,40 @@ blogsRouter.post('/', userExtractor, async (request, response) => {
     url,
     likes,
     user: user._id,
-  });
+  })
 
-  let savedBlog = await blog.save();
-  savedBlog = await savedBlog.populate('user', { username: 1, name: 1 });
+  let savedBlog = await blog.save()
+  savedBlog = await savedBlog.populate('user', { username: 1, name: 1 })
 
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
-  response.status(201).json(savedBlog);
-});
+  response.status(201).json(savedBlog)
+})
 
 blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id);
+  const blog = await Blog.findById(request.params.id)
   if (blog) {
-    response.json(blog);
+    response.json(blog)
   } else {
-    response.status(404).end();
+    response.status(404).end()
   }
-});
+})
 
 blogsRouter.put('/:id', userExtractor, async (request, response) => {
-  const { title, author, url, likes } = request.body;
-  const blogId = request.params.id;
+  const { title, author, url, likes } = request.body
+  const blogId = request.params.id
 
-  const user = request.user;
-  const blog = await Blog.findById(blogId);
+  const user = request.user
+  const blog = await Blog.findById(blogId)
 
   if (!blog) {
-    return response.status(404).end();
+    return response.status(404).end()
   }
 
   const updateFields = blog.user.equals(user._id)
     ? { title, author, url, likes }
-    : { likes };
+    : { likes }
 
   let updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
@@ -63,34 +63,34 @@ blogsRouter.put('/:id', userExtractor, async (request, response) => {
       runValidators: true,
       context: 'query',
     }
-  ).populate('user', { username: 1, name: 1 });
+  ).populate('user', { username: 1, name: 1 })
 
   if (updatedBlog) {
-    response.json(updatedBlog);
+    response.json(updatedBlog)
   } else {
-    response.status(404).end();
+    response.status(404).end()
   }
-});
+})
 
 blogsRouter.delete('/:id', userExtractor, async (request, response) => {
-  const blogId = request.params.id;
+  const blogId = request.params.id
 
-  const user = request.user;
-  const blog = await Blog.findById(blogId);
+  const user = request.user
+  const blog = await Blog.findById(blogId)
 
   if (!blog) {
-    return response.status(404).end();
+    return response.status(404).end()
   }
 
   if (!blog.user.equals(user._id)) {
-    return response.status(401).json({ error: 'unauthorized operation' });
+    return response.status(401).json({ error: 'unauthorized operation' })
   }
 
-  user.blogs = user.blogs.filter((id) => id.toString() !== request.params.id);
-  await user.save();
+  user.blogs = user.blogs.filter((id) => id.toString() !== request.params.id)
+  await user.save()
 
-  await Blog.findByIdAndDelete(blogId);
-  response.status(204).end();
-});
+  await Blog.findByIdAndDelete(blogId)
+  response.status(204).end()
+})
 
-module.exports = blogsRouter;
+module.exports = blogsRouter
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,76 +1,76 @@
-const jwt = require('jsonwebtoken');
-const morgan = require('morgan');
-const chalk = require('chalk');
-const logger = require('./logger');
-const User = require('../models/user');
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
+const chalk = require('chalk')
+const logger = require('./logger')
+const User = require('../models/user')
 
-morgan.token('post-body', (req, res) => JSON.stringify(req.body));
+morgan.token('post-body', (req, res) => JSON.stringify(req.body))
 const requestLogger = morgan((tokens, req, res) => {
   return [
     chalk.cyan(tokens.method(req, res)),
     chalk.greenBright(tokens.url(req, res)),
     chalk.magentaBright(tokens.status(req, res)),
     chalk.grey(tokens['response-time'](req, res) + ' ms'),
-    chalk.whiteBright(tokens['post-body'](req, res))
-  ].join(' ');
-});
+    chalk.whiteBright(tokens['post-body'](req, res)),
+  ].join(' ')
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message);
+  logger.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   } else if (
     error.name === 'MongoServerError' &&
     error.message.includes('E11000 duplicate key error')
   ) {
     return response
       .status(400)
-      .json({ error: 'expected `username` to be unique' });
+      .json({ error: 'expected `username` to be unique' })
   } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({ error: 'token invalid' });
+    return response.status(401).json({ error: 'token invalid' })
   } else if (error.name === 'TokenExpiredError') {
     return response.status(401).json({
-      error: 'token expired'
-    });
+      error: 'token expired',
+    })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 const getTokenFromRequest = (request) => {
-  const authorization = request.get('authorization');
+  const authorization = request.get('authorization')
 
   if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '');
+    return authorization.replace('Bearer ', '')
   }
 
-  return null;
-};
+  return null
+}
 
 const userExtractor = async (request, response, next) => {
-  const token = getTokenFromRequest(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
+  const token = getTokenFromRequest(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
 
   if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' });
+    return response.status(401).json({ error: 'token invalid' })
   }
 
-  const user = await User.findById(decodedToken.id);
-  request.user = user;
+  const user = await User.findById(decodedToken.id)
+  request.user = user
 
-  next();
-};
+  next()
+}
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  userExtractor
-};
+  userExtractor,
+}
