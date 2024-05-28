32283501079c21caefa28f48b2991c7da72256cb
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-28 10:19:38 +0300

    modify the creation of new anecdotes, so that anecdotes are stored in the backend

--- part6/anecdotes-redux/src/components/AnecdoteForm.jsx
+++ part6/anecdotes-redux/src/components/AnecdoteForm.jsx
@@ -1,16 +1,20 @@
 import { useDispatch } from 'react-redux'
 
+import anecdoteService from '../services/anecdotes'
 import { create } from '../reducers/anecdoteReducer'
 import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
     e.target.anecdote.value = ''
-    dispatch(create(content))
+
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(create(newAnecdote))
+
     dispatch(showNotification(`You have created '${content}'`))
     setTimeout(() => {
       dispatch(showNotification(null))
