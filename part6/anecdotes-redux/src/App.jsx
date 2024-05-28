4fe3537984bc9246d26497a59fe97c4aaf965220
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-28 10:15:39 +0300

    fetch anecdotes from db.json and add them to the store

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -1,9 +1,20 @@
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+
+import anecdoteService from './services/anecdotes'
+import { set } from './reducers/anecdoteReducer'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => dispatch(set(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
