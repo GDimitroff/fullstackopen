2c15ff47256be2982519ed3ae42f392643136cb9
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-29 18:06:18 +0300

    split app component into separate components

---
+++ part7/routed-anecdotes/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <div>
+    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+    See <a href='https://example.com'>https://example.com</a> for the source
+    code.
+  </div>
+)
+
+export default Footer
