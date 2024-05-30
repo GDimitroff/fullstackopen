dbe627a4bcb328f2f916b84c2b6c4b9b448bee51
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-05-30 10:01:35 +0300

    add useField hook

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useField } from '../hooks'
 
 const CreateNew = ({ addNew }) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,10 +14,6 @@ const CreateNew = ({ addNew }) => {
       info,
       votes: 0,
     })
-
-    setContent('')
-    setAuthor('')
-    setInfo('')
   }
 
   return (
@@ -26,27 +22,15 @@ const CreateNew = ({ addNew }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
