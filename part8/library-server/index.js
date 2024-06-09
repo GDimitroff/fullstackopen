1266c5fc83153dbf9298ffb4ae5b5389d2d95324
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-09 17:49:44 +0300

    setup initial mongodb connection, add book, author models

--- part8/library-server/index.js
+++ part8/library-server/index.js
@@ -1,8 +1,25 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
 import { typeDefs } from './schema.js'
 import { resolvers } from './resolvers.js'
+
+mongoose.set('strictQuery', false)
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 const server = new ApolloServer({
   typeDefs,
