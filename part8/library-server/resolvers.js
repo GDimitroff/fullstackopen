1a384b304640bfbb7212327c979ded95fd3fbcc0
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-10 12:12:10 +0300

    make all queries and mutations work

--- part8/library-server/resolvers.js
+++ part8/library-server/resolvers.js
@@ -1,84 +1,6 @@
 import Book from './models/book.js'
 import Author from './models/author.js'
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky',
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz',
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-]
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-]
-
 export const resolvers = {
   Query: {
     bookCount: async () => Book.countDocuments(),
@@ -106,19 +28,17 @@ export const resolvers = {
       }
 
       const book = new Book({ ...args, author: foundAuthor._id })
-      return book.save()
+      await book.save()
+      return book.populate('author')
     },
-    editAuthor: (_, args) => {
-      const { name, setBornTo } = args
-      const author = authors.find((a) => a.name === name)
+    editAuthor: async (_, { name, setBornTo }) => {
+      const author = await Author.findOneAndUpdate(
+        { name },
+        { born: setBornTo },
+        { new: true }
+      )
 
-      if (!author) {
-        return null
-      }
-
-      const updatedAuthor = { ...author, born: setBornTo }
-      authors = authors.map((a) => (a.name === name ? updatedAuthor : a))
-      return updatedAuthor
+      return author
     },
   },
 
