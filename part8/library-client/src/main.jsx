c9ec503766dfeaae03465822b662c68facd292d3
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-13 10:59:10 +0300

    add code comments for subscriptions in the client

--- part8/library-client/src/main.jsx
+++ part8/library-client/src/main.jsx
@@ -32,6 +32,11 @@ const wsLink = new GraphQLWsLink(
   createClient({ url: 'ws://localhost:4000/graphql' })
 )
 
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query)
