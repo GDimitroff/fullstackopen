75ad23930453b8a688dd20bcc6bd142a652e4351
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-15 15:44:46 +0300

    bmi calculator now can receive cli arguments

--- part9/learntypescript/bmiCalculator.ts
+++ part9/learntypescript/bmiCalculator.ts
@@ -1,3 +1,8 @@
+interface BMIValues {
+  height: number
+  weight: number
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / Math.pow(height / 100, 2)
 
@@ -12,4 +17,30 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-console.log(calculateBmi(176, 77))
+const parseArguments = (args: Array<string>): BMIValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv)
+  const result = calculateBmi(height, weight)
+
+  console.log(result)
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+
+  console.log(errorMessage)
+}
