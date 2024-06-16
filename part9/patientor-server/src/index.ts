fb08c186719a1cc134fde2b675c9a43796fdd4f2
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-16 14:41:05 +0300

    fix eslint/prettier

--- part9/patientor-server/src/index.ts
+++ part9/patientor-server/src/index.ts
@@ -9,9 +9,6 @@ app.get('/api/ping', (_req, res) => {
 const PORT = 3003
 const ENV = process.env.NODE_ENV
 
-const array2 = [1, 2, 3, 4, 5]
-array2.map((d) => console.log(d))
-
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT} on ${ENV}`)
 })
