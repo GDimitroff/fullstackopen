293683582d3f19886126272a1904a27a10c0cc7f
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-16 12:32:27 +0300

    add eslint, prettier configs

--- part9/patientor-server/src/index.ts
+++ part9/patientor-server/src/index.ts
@@ -3,13 +3,12 @@ import express from 'express'
 const app = express()
 
 app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here!!')
-  res.send('pong')
+	res.send('pong')
 })
 
 const PORT = 3003
+const ENV = process.env.NODE_ENV
 
 app.listen(PORT, () => {
-  console.log(process.env.NODE_ENV)
-  console.log(`🚀 Server running on port ${PORT}`)
+	console.log(`🚀 Server running on port ${PORT} on ${ENV}`)
 })
