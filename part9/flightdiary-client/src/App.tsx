465ab7d0e411b22f4f579d1c39b66b1c02983a8c
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-17 22:11:23 +0300

    implement get all diaries from the backned, adjust stuff

--- part9/flightdiary-client/src/App.tsx
+++ part9/flightdiary-client/src/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react'
+
+import diaryService from './services/diaryService'
+import { DiaryEntry } from './types'
+
 const App = () => {
+  const [diaries, setDiaries] = useState<DiaryEntry[] | null>(null)
+
+  useEffect(() => {
+    const fetchDiaries = async () => {
+      const diaries = await diaryService.getAll()
+      setDiaries(diaries)
+    }
+
+    void fetchDiaries()
+  }, [])
+
+  if (!diaries) return null
+
   return (
     <div>
-      <h1>Flight Diary</h1>
+      <h1>Diary entries</h1>
+      {diaries.map((diary) => {
+        return (
+          <div key={diary.id}>
+            <h2>
+              <strong>{diary.date}</strong>
+            </h2>
+            <p>{diary.comment}</p>
+            <p>visibility: {diary.visibility}</p>
+            <p>weather: {diary.weather}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
