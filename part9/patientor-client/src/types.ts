6e836f73ddc07c105084d12f50b46c4fd3c4d4d2
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-06-18 16:16:08 +0300

    adjust frontend to receive the complete patients data

--- part9/patientor-client/src/types.ts
+++ part9/patientor-client/src/types.ts
@@ -1,4 +1,50 @@
-interface Entry {}
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never
+
+interface BaseEntry {
+  id: string
+  description: string
+  date: string
+  specialist: string
+  diagnosisCodes?: Array<Diagnosis['code']>
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck'
+  healthCheckRating: HealthCheckRating
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare'
+  employerName: string
+  sickLeave?: {
+    startDate: string
+    endDate: string
+  }
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital'
+  discharge: {
+    date: string
+    criteria: string
+  }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>
 
 export interface Diagnosis {
   code: string
@@ -17,8 +63,8 @@ export interface Patient {
   name: string
   occupation: string
   gender: Gender
+  dateOfBirth: string
   ssn?: string
-  dateOfBirth?: string
   entries?: Entry[]
 }
 
