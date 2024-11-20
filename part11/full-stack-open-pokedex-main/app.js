ea3af69e2e2ef01e8d59456465a92cb19a52fe85
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-20 15:59:53 +0200

    deploy: test intentional failure

--- part11/full-stack-open-pokedex-main/app.js
+++ part11/full-stack-open-pokedex-main/app.js
@@ -6,9 +6,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('4')
+  res.send('5')
 })
 
 app.listen(PORT, () => {
+  this_causes_error
   console.log(`server started on port ${PORT}`)
 })
