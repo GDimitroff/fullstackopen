909e086f4afd2c029f7e204783d56e9a3ed655f1
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-20 09:08:23 +0200

    tests: remove all browsers except chromium

--- part11/full-stack-open-pokedex-main/playwright.config.js
+++ part11/full-stack-open-pokedex-main/playwright.config.js
@@ -37,16 +37,6 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
   webServer: {
     command: 'npm run start',
