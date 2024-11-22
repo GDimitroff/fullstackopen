aacb1cd2d294b0c235e94cd67a0419294088a4ab
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-22 09:30:27 +0200

    ci/cd: feature branch worklow testing

--- part11/full-stack-open-pokedex-main/app.js
+++ part11/full-stack-open-pokedex-main/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('21')
+  res.send('22')
 })
 
 app.get('/health', (req, res) => {
