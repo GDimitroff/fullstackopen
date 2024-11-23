886b7eba2781438eaefa030d1d0761cdc7e9d525
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-23 17:43:18 +0200

    ci/cd: test if this will skip deployment #skip

--- part11/full-stack-open-pokedex-main/app.js
+++ part11/full-stack-open-pokedex-main/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('41')
+  res.send('42')
 })
 
 app.get('/health', (req, res) => {
