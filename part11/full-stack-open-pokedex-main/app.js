470ca99292501c04fa0121d610737a377ce699c3
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-23 13:55:10 +0200

    ci/cd: setup separate job for tag release

--- part11/full-stack-open-pokedex-main/app.js
+++ part11/full-stack-open-pokedex-main/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('28')
+  res.send('29')
 })
 
 app.get('/health', (req, res) => {
