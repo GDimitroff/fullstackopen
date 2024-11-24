dc53b0406bc5d46552fb0e88a6f123f01619bf8b
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-24 16:11:31 +0200

    ci/cd: discord notification on build failure

--- part11/full-stack-open-pokedex-main/app.js
+++ part11/full-stack-open-pokedex-main/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('51')
+  res.send('52')
 })
 
 app.get('/health', (req, res) => {
