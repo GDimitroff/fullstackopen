21f916e8a01c7d5fc346c5e88f213a65033a326c
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-25 10:18:11 +0200

    ci/cd: discord notification on successful deploy

--- part11/full-stack-open-pokedex-main/app.js
+++ part11/full-stack-open-pokedex-main/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('61')
+  res.send('62')
 })
 
 app.get('/health', (req, res) => {
