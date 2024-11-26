dc5184a8c859739bca965b0e760a3559b5b619ed
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-26 10:09:18 +0200

    ci/cd: health check will fail

--- part11/full-stack-open-pokedex-main/app.js
+++ part11/full-stack-open-pokedex-main/app.js
@@ -6,14 +6,16 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('64')
+  res.send('65')
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-undef
+  throw new Error('oh no')
+  // eslint-disable-next-line
   res.send('ok')
 })
 
-// TODO: + 1 simulates health check error
 app.listen(PORT + 1, () => {
   console.log(`server started on port ${PORT}`)
 })
