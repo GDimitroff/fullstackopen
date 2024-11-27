53a979c8e0ea1e17a167a4cccf1ef77d175b8948
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-27 10:54:17 +0200

    part11: setup the server

---
+++ part11/bloglist/controllers/users.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcrypt')
+const { Router } = require('express')
+const User = require('../models/user')
+
+const usersRouter = Router()
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', {
+    url: 1,
+    title: 1,
+    author: 1,
+  })
+
+  response.json(users)
+})
+
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  })
+
+  if (user) {
+    response.json(user)
+  } else {
+    response.status(404).end()
+  }
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  if (!password) {
+    return response.status(400).json({
+      error: `User validation failed: password is required`,
+    })
+  }
+
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: `User validation failed: password is shorter than the minimum allowed length (3)`,
+    })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
---
+++ part11/bloglist/tests/users_api.test.js
@@ -0,0 +1,147 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const helper = require('./helper')
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await helper.initializeTestDatabase()
+  })
+
+  test('creation succeeds with a valid user data', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'GDimitroff',
+      name: 'Gospodin Dimitrov',
+      password: 'bookworm',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((u) => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'King',
+      name: 'Superuser',
+      password: 'admin',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+
+  test('creation fails with proper statuscode and message if username is missing', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      name: 'Gospodin Dimitrov',
+      password: 'bookworm',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('`username` is required'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+
+  test('creation fails with proper statuscode and message if username is too short', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'GD',
+      name: 'Gospodin Dimitrov',
+      password: 'bookworm',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('is shorter than the minimum allowed length (3)'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+
+  test('creation fails with proper statuscode and message if password is missing', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'GDimitroff',
+      name: 'Gospodin Dimitrov',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    assert(result.body.error.includes('User validation failed: password is required'))
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+
+  test('creation fails with proper statuscode and message if password too short', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'GDimitroff',
+      name: 'Gospodin Dimitrov',
+      password: 'bo',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    assert(
+      result.body.error.includes(
+        'User validation failed: password is shorter than the minimum allowed length (3)',
+      ),
+    )
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
