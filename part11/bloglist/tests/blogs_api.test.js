3d852a5a49a7bc311e24fb7a9f7f979e39868b5d
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-11-29 17:10:33 +0200

    ci/cd: bloglist workflow test

--- part11/bloglist/tests/blogs_api.test.js
+++ part11/bloglist/tests/blogs_api.test.js
@@ -75,12 +75,6 @@ describe('when there is initially some blogs saved', () => {
 
   describe('addition of a new blog', () => {
     test('succeeds with valid data', async () => {
-      console.log(token)
-      const response = await api.post('/api/login').send({ username: 'King', password: 'king' })
-
-      const token2 = response.body.token
-      console.log(token2)
-
       const users = await helper.usersInDb()
       const kingUser = users.find((u) => u.username === 'King')
 
@@ -94,7 +88,7 @@ describe('when there is initially some blogs saved', () => {
 
       await api
         .post('/api/blogs')
-        .set('Authorization', `Bearer ${token2}`)
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
