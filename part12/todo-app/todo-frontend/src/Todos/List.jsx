aa591b50145cbff85407cdd1bd5149ecbf8e2ef7
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2024-12-16 10:31:20 +0200

    part12: setup unit tests

--- part12/todo-app/todo-frontend/src/Todos/List.jsx
+++ part12/todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,4 +1,45 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+
+export const Todo = ({ todo, onClickDelete, onClickComplete }) => {
+  const doneInfo = (
+    <>
+      <span>This todo is done</span>
+      <span>
+        <button onClick={onClickDelete(todo)}> Delete </button>
+      </span>
+    </>
+  )
+
+  const notDoneInfo = (
+    <>
+      <span>This todo is not done</span>
+      <span>
+        <button onClick={onClickDelete(todo)}> Delete </button>
+        <button onClick={onClickComplete(todo)}> Set as done </button>
+      </span>
+    </>
+  )
+
+  return (
+    <div>
+      <hr />
+      <div
+        className='todo'
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          maxWidth: '70%',
+          margin: 'auto',
+        }}
+      >
+        <span>{todo.text}</span>
+        {todo.done ? doneInfo : notDoneInfo}
+      </div>
+    </div>
+  )
+}
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   const onClickDelete = (todo) => () => {
@@ -10,45 +51,16 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   }
 
   return (
-    <>
-      {todos
-        .map((todo) => {
-          const doneInfo = (
-            <>
-              <span>This todo is done</span>
-              <span>
-                <button onClick={onClickDelete(todo)}> Delete </button>
-              </span>
-            </>
-          )
-
-          const notDoneInfo = (
-            <>
-              <span>This todo is not done</span>
-              <span>
-                <button onClick={onClickDelete(todo)}> Delete </button>
-                <button onClick={onClickComplete(todo)}> Set as done </button>
-              </span>
-            </>
-          )
-
-          return (
-            <div
-              key={todo.id}
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                maxWidth: '70%',
-                margin: 'auto',
-              }}
-            >
-              <span>{todo.text}</span>
-              {todo.done ? doneInfo : notDoneInfo}
-            </div>
-          )
-        })
-        .reduce((acc, cur) => [...acc, <hr />, cur], [])}
-    </>
+    <div>
+      {todos.map((todo) => (
+        <Todo
+          key={todo._id}
+          todo={todo}
+          onClickDelete={onClickDelete}
+          onClickComplete={onClickComplete}
+        />
+      ))}
+    </div>
   )
 }
 
