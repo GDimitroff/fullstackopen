fd6e1190059ba208d59a72045e133d157bcd19aa
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-06 15:41:14 +0200

    part13: add blogFinder middleware

--- part13/bloglist/controllers/blogs.js
+++ part13/bloglist/controllers/blogs.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
@@ -16,41 +21,36 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    res.json(blog)
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog)
   } else {
-    res.status(404).end()
+    res.status(404).json({ error: 'Blog not found' })
   }
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', blogFinder, async (req, res) => {
   try {
-    const blog = await Blog.findByPk(req.params.id)
-
-    if (!blog) {
+    if (!req.blog) {
       return res.status(404).json({ error: 'Blog not found' })
     }
 
-    await blog.update(req.body)
+    await req.blog.update(req.body)
 
-    res.json(blog)
+    res.json(req.blog)
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal server error' })
   }
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', blogFinder, async (req, res) => {
   try {
-    const blog = await Blog.findByPk(req.params.id)
-
-    if (!blog) {
+    if (!req.blog) {
       return res.status(404).json({ error: 'Blog not found' })
     }
 
-    await blog.destroy()
+    await req.blog.destroy()
     res.status(204).end()
   } catch (error) {
     console.error(error)
