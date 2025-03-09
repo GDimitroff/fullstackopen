7e14d4b0e7047ca1d1f98e5f9d20e7b32ca3606a
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-09 16:23:01 +0200

    part13: adjust initial migration

--- part13/bloglist/migrations/20250309_00_initialize_blogs_and_users.js
+++ part13/bloglist/migrations/20250309_00_initialize_blogs_and_users.js
@@ -53,12 +53,10 @@ module.exports = {
       created_at: {
         type: DataTypes.DATE,
         allowNull: false,
-        defaultValue: DataTypes.NOW,
       },
       updated_at: {
         type: DataTypes.DATE,
         allowNull: false,
-        defaultValue: DataTypes.NOW,
       },
     })
 
