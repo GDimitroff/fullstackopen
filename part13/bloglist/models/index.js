72835e5a94de4efb64e324c340002aea9f8986a6
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-10 14:31:34 +0200

    part13: exercise 13.19

--- part13/bloglist/models/index.js
+++ part13/bloglist/models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog')
 const User = require('./user')
+const Reading = require('./reading')
 
-User.hasMany(Blog)
 Blog.belongsTo(User)
+User.hasMany(Blog)
+
+User.belongsToMany(Blog, { through: Reading })
+Blog.belongsToMany(User, { through: Reading })
 
 module.exports = {
   Blog,
   User,
+  Reading,
 }
