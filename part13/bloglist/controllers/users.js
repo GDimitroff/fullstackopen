bd5ac659a099233b0473dd7e032cb8a32ed71402
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-10 17:50:19 +0200

    part13: exercise 13.21

--- part13/bloglist/controllers/users.js
+++ part13/bloglist/controllers/users.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
         model: Blog,
         as: 'readings',
         attributes: { exclude: ['userId'] },
-        through: { attributes: [] },
+        through: { attributes: ['id', 'read'] },
       },
     ],
   })
