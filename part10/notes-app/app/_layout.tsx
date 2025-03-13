6ee7d6779818b07658236274d82bae6d3cddff77
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-13 14:18:17 +0200

    part10: add home screen

--- part10/notes-app/app/_layout.tsx
+++ part10/notes-app/app/_layout.tsx
@@ -1,5 +1,30 @@
 import { Stack } from 'expo-router'
 
-export default function RootLayout() {
-  return <Stack />
+const RootLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'tomato',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: 'bold',
+        },
+        contentStyle: {
+          backgroundColor: '#fff',
+        },
+      }}
+    >
+      <Stack.Screen
+        name='index'
+        options={{
+          title: 'Home',
+        }}
+      />
+    </Stack>
+  )
 }
+
+export default RootLayout
--- part10/notes-app/app/index.tsx
+++ part10/notes-app/app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { useRouter } from 'expo-router'
 
-export default function Index() {
+const HomeScreen = () => {
+  const router = useRouter()
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require('@/assets/images/post-it.png')} style={styles.image} />
+      <Text style={styles.title}>Welcome To Notes App</Text>
+      <Text style={styles.subtitle}>Capture your thoughts anytime, anywhere</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => router.push('/notes')}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f9fa',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'tomato',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+})
+
+export default HomeScreen
