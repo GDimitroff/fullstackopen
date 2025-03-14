7923591b7f96e50e872fdff05c782626faae8064
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-14 17:44:05 +0200

    part10: add auth context and provider

--- part10/notes-app/app/_layout.tsx
+++ part10/notes-app/app/_layout.tsx
@@ -1,37 +1,41 @@
 import { Stack } from 'expo-router'
 
+import { AuthProvider } from '@/contexts/AuthContext'
+
 const RootLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: 'tomato',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontSize: 20,
-          fontWeight: 'bold',
-        },
-        contentStyle: {
-          paddingTop: 10,
-          paddingHorizontal: 10,
-          backgroundColor: '#fff',
-        },
-      }}
-    >
-      <Stack.Screen
-        name='index'
-        options={{
-          title: 'Home',
+    <AuthProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'tomato',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: 'bold',
+          },
+          contentStyle: {
+            paddingTop: 10,
+            paddingHorizontal: 10,
+            backgroundColor: '#fff',
+          },
         }}
-      />
-      <Stack.Screen
-        name='notes'
-        options={{
-          headerTitle: 'Notes',
-        }}
-      />
-    </Stack>
+      >
+        <Stack.Screen
+          name='index'
+          options={{
+            title: 'Home',
+          }}
+        />
+        <Stack.Screen
+          name='notes'
+          options={{
+            headerTitle: 'Notes',
+          }}
+        />
+      </Stack>
+    </AuthProvider>
   )
 }
 
