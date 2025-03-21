f9e7bbdad6304f5a9b3132df899d9e5bb3bc6375
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-21 16:27:06 +0200

    part10: enhance apollo clients requests

--- part10/rate-repository-app/client/src/utils/apolloClient.js
+++ part10/rate-repository-app/client/src/utils/apolloClient.js
@@ -1,11 +1,35 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 import Constants from 'expo-constants'
+import { setContext } from '@apollo/client/link/context'
 
-const createApolloClient = () => {
-  const apolloUri = Constants.expoConfig.extra.apolloUri
+const { apolloUri } = Constants.expoConfig.extra
+
+const httpLink = createHttpLink({
+  uri: apolloUri,
+})
+
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken()
+
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      }
+    } catch (e) {
+      console.log(e)
+
+      return {
+        headers,
+      }
+    }
+  })
 
   return new ApolloClient({
-    uri: apolloUri,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   })
 }
