f6ea6bdad715de71ec25f9df10bcc715893b0a97
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2025-03-22 14:47:21 +0200

    part10: adjust useRepository fetch policy

--- part10/rate-repository-app/client/src/hooks/useRepository.jsx
+++ part10/rate-repository-app/client/src/hooks/useRepository.jsx
@@ -4,6 +4,7 @@ import { GET_REPOSITORY } from '../graphql/queries'
 const useRepository = (id) => {
   const { data, error, loading } = useQuery(GET_REPOSITORY, {
     variables: { id },
+    fetchPolicy: 'cache-and-network',
   })
 
   return { data: data?.repository, loading, error }
